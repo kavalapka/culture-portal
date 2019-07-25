6dc464691a5e3d60c8725e02281675db1dd26b76
Katerina Zenchik <e.zenchik@gmail.com>
2019-07-25 12:44:27 +0300

    add: react-bootstrap

--- culture-portal/src/pages/index.js
+++ culture-portal/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Button } from 'react-bootstrap';
 
 import Layout from '../components/layout';
 import Image from '../components/image';
@@ -9,6 +10,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <Button>I am Button from Bootstrap</Button>
+    <button>Another button</button>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
