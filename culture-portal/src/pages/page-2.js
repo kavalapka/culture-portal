fd0b799a0a7cc1df0c5cb19a2bfecdfb701c3d65
Katerina Zenchik <e.zenchik@gmail.com>
2019-07-25 12:44:27 +0300

    add: react-bootstrap

--- culture-portal/src/pages/page-2.js
+++ culture-portal/src/pages/page-2.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Button } from 'react-bootstrap';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -8,7 +9,7 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <Button>Welcome to page 2</Button>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 );
