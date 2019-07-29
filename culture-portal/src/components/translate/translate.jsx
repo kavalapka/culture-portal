9567dd3107dd6bd9ffb16a38ce0ff391523171f0
juliastetskaya <juliastetskaya@gmail.com>
2019-07-29 11:29:32 +0300

    feat: add state for translate

--- culture-portal/src/components/translate/translate.jsx
+++ culture-portal/src/components/translate/translate.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { useTranslation } from 'react-i18next';
 
 const Translate = () => {
   const { t, i18n } = useTranslation();
+  const [value, setCount] = useState(i18n.language);
 
   const changeLanguage = (locale) => {
     i18n.changeLanguage(locale);
@@ -11,12 +12,13 @@ const Translate = () => {
 
   const handleChange = (event) => {
     changeLanguage(`${event.target.value}`);
+    setCount(`${event.target.value}`);
   };
 
   return (
     <Form className="select-language">
       <Form.Group>
-        <Form.Control as="select" onChange={handleChange}>
+        <Form.Control as="select" value={value} onChange={handleChange}>
           <option value="ru">{t('russian')}</option>
           <option value="be">{t('belorussian')}</option>
           <option value="en">{t('english')}</option>
