852238100b2c318b0a8967f0beb38054a2fadbc7
LoraBard <bard.lora@mail.ru>
2019-07-29 14:43:06 +0300

    style: change style of header

--- culture-portal/src/components/header/header.jsx
+++ culture-portal/src/components/header/header.jsx
@@ -1,31 +1,32 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { Link } from 'gatsby';
-import { useTranslation } from 'react-i18next';
 import Translate from '../translate';
 import logo from '../../images/logo.png';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const Header = () => {
-  const { t } = useTranslation();
-  return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-light bottom-sticky">
-        <img src={logo} alt="logo" className="navbar-brand mb-0" />
-        <div>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu" aria-expanded="false">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="mainMenu">
-            <ul className="navbar-nav mr-auto ml-auto mt-2 mt-lg-0">
-              <li className="nav-item active mb-0"><Link to="/" activeStyle={{ color: '#fff' }}>{t('menu.main')}</Link></li>
-              <li className="nav-item mb-0"><Link to="/search/" activeStyle={{ color: '#fff' }}>{t('menu.search')}</Link></li>
-            </ul>
-            <Translate />
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
-};
+const Header = () => (
+  <header>
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <Link to="/"><img src={logo} alt="logo" className="navbar-brand mb-0" /></Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto mt-4">
+          <li className="nav-item active">
+            <Link to="/" activeStyle={{ color: '#fff' }}>
+              Главная
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/search" activeStyle={{ color: '#fff' }}>Поиск</Link>
+          </li>
+          <li className="nav-item"><Translate /></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
 
 export default Header;
