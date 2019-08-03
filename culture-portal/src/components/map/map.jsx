5a094025b771aaf8c7c9a7db4400a5bfbd11955a
Viktar Hlushchenia <slayer110@yandex.ru>
2019-08-03 16:25:22 +0300

    refactor: delete array places from map

--- culture-portal/src/components/map/map.jsx
+++ culture-portal/src/components/map/map.jsx
@@ -7,27 +7,7 @@ import {
 } from 'react-google-maps';
 
 
-const places = [
-  {
-    id: '1',
-    title: 'Varshava',
-    name: 'Varshava',
-    lat: 52.22977,
-    lng: 21.01178,
-  },
-  {
-    id: '2',
-    title: 'Giżycko',
-    lat: 54.0381100,
-    lng: 21.7644100,
-  },
-  {
-    id: '3',
-    title: 'Minsk',
-    lat: 53.9,
-    lng: 27.56659,
-  },
-];
+
 
 
 const MapComponent = withScriptjs(withGoogleMap(props => (
