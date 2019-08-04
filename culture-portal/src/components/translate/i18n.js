7132fe76194cf6bb7d7ab72c7c851b0bb4407445
Katerina Zenchik <e.zenchik@gmail.com>
2019-08-04 12:02:36 +0300

    deploy: prepare to deploy on gh-pages

--- culture-portal/src/components/translate/i18n.js
+++ culture-portal/src/components/translate/i18n.js
@@ -1,15 +1,9 @@
 import i18n from 'i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import jsonEN from '../../assets/i18n/en.json';
 import jsonBY from '../../assets/i18n/by.json';
 import jsonRU from '../../assets/i18n/ru.json';
 
-function getLanguage() {
-  // eslint-disable-next-line no-undef
-  const lng = window.location.pathname.split('/')[1];
-  return lng || 'ru';
-}
 
 i18n
   // .use(LanguageDetector) // use to set basic language of user's browser language
@@ -27,9 +21,9 @@ i18n
         translations: jsonRU,
       },
     },
-    lng: getLanguage(),
+    lng: 'ru',
     fallbackLng: 'ru',
-    debug: true,
+    debug: false,
 
     // have a common namespace used around the full app
     ns: ['translations'],
