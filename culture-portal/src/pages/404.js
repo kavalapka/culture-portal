5e4140fc5ccaf7bed90dd075a3580d04244c6752
Dzmitry Ivanou <3prn@mail.ru>
2019-08-04 13:13:44 +0300

    feat: implement page 404

--- culture-portal/src/pages/404.js
+++ culture-portal/src/pages/404.js
@@ -1,14 +1,23 @@
-import React from 'react';
-
-import Layout from '../components/layout';
+import React, { Fragment } from 'react';
+import { Link } from 'gatsby';
+import { useTranslation } from 'react-i18next';
 import SEO from '../components/seo';
+import '../components/style/404Styles.css';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+  return (
+    <div id="main404">
+      <h1>404</h1>
+      <SEO title="404: Not found" />
+      <h3>{t('error404')}</h3>
+      <Fragment>
+        <h5>{t('description404')}</h5>
+        <p className="intro">{t('intro')}</p>
+      </Fragment>
+      <Link className="btn-primary" to="/">Go back to the homepage</Link>
+    </div>
+  );
+};
 
 export default NotFoundPage;
